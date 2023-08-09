8355fd97b97547525d7330c39ddba24ca26f51b3
Kyle A Pearson <kyle.a.pearson@jpl.nasa.gov>
2023-08-09 14:00:39 -0700

    eval scripts

---
+++ eval_rock.py
@@ -0,0 +1,184 @@
+import os
+import cv2
+import glymur
+import argparse
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from scipy.ndimage import label
+from skimage.util.shape import view_as_windows
+from skimage.transform import resize
+
+from trainer import train_ensembler
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--image", type=str, default="images/ESP_016287_2205_RED_A_01_ORTHO.JP2", help="Choose a JPEG2000 image to decode")
+
+    parser.add_argument("-r", "--res", type=int, default=0, help="Resolution level to decode (0 is highest resolution)")
+
+    parser.add_argument("-th", "--threads", help="number of threads for background class", default=8, type=int)
+
+    parser.add_argument("-tr", "--train", help="training data for rocks", default="images/training_data_121.csv", type=str)
+
+    parser.add_argument("-te", "--test", help="testing data for rocks", default="images/training_data_121.csv", type=str)
+
+    parser.add_argument("-o", "--outdir", type=str, default="output/", help="Directory to save output images")
+
+    parser.add_argument("-p", "--plot", help="plot results", action='store_true')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    # read args from cmd line
+    args = parse_args()
+
+    glymur.set_option('lib.num_threads', args.threads)
+
+    print("Training ensemble...")
+    ensemble = train_ensembler(args.train, args.test)
+    print(ensemble.acc)
+
+    print('Reading image...')
+    image = glymur.Jp2k(args.image).read(rlevel=0).astype(np.float32)
+
+    # set up output
+    outdir = os.path.join(args.outdir, os.path.splitext(os.path.basename(args.image))[0])
+
+    # make dirs
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    # 11x11 window size
+    ws = int(ensemble.xdim**0.5)
+
+    # create boolean mask same size as image
+    mask = np.zeros(image.shape, dtype=bool)
+
+    # break into 512x512 windows for evaluation
+    #BI = view_as_windows(image, (512, 512), 512)
+    #BIR = BI.reshape(-1, 512, 512) # reshape to 1D array
+    #for i in tqdm(range(BIR.shape[0])):
+    eval_size = 512
+
+    # create double for loop for slicing image into 512x512 windows
+    for i in tqdm(range(0, image.shape[0], eval_size)):
+        for j in range(0, image.shape[1], eval_size):
+
+            # slice image into 512x512 window
+            window = image[i:i+eval_size, j:j+eval_size]
+
+            # skip if window is all zeros
+            if window.max() == 0:
+                continue
+
+            # break into 11x11 windows
+            BI_sub = view_as_windows(window, (ws, ws), 1)
+            BIR_sub = BI_sub.reshape(-1, ws**2)
+
+            # compute some size stuff for reconstruction
+            patch_image = np.array([BI_sub.shape[0], BI_sub.shape[1]])
+            pad = [int((window.shape[0] - patch_image[0])*0.5), int((window.shape[1] - patch_image[1])*0.5)]
+
+            # preprocess each sample, scale between 0 - 1
+            patches_flat = BIR_sub.reshape(BIR_sub.shape[0], -1)
+            patches_pre = (patches_flat - patches_flat.mean(axis=1, keepdims=True)) / (1+patches_flat.std(axis=1, keepdims=True))
+
+            # apply scaler as generic filter to image
+            center_pixel = int((ws**2)/2)
+            image_pre = patches_pre[:,center_pixel].reshape(BI_sub.shape[0], BI_sub.shape[1])
+
+            # get predictions
+            y_prob, y_std = ensemble.predict(BIR_sub, prob=True)
+            #y_prob = ensemble.predict_best(BIR_sub, prob=True)
+
+            # reshape to 512x512ish
+            y_prob = y_prob[:,1].reshape(BI_sub.shape[0], BI_sub.shape[1])
+            y_prob = resize(y_prob, patch_image, order=0, preserve_range=True, anti_aliasing=False)
+
+            # pad to get to 512x512
+            y_prob = np.pad(y_prob, ((pad[0],pad[0]), (pad[1],pad[1])), 'constant', constant_values=0)
+
+            # save y_prob back to mask
+            mask[i:i+eval_size, j:j+eval_size] = y_prob > 0.6 # TODO parameterize threshold?
+
+            if args.plot:
+                # create plot
+                fig, ax_local = plt.subplots(3,2, figsize=(12,12))
+                ax_local[0,0].imshow(window, cmap='gray')
+                ax_local[0,1].imshow(image_pre, cmap='gray')
+                ax_local[1,1].imshow(window, cmap='gray') # rock labels
+                ax_local[1,0].imshow(window, cmap='gray') # probability map
+
+                im = ax_local[1,0].imshow(y_prob, cmap='jet', alpha=0.5)
+                cbar = fig.colorbar(im, ax=ax_local[1,0], fraction=0.046, pad=0.04)
+                cbar.set_label('Probability of Rock', rotation=270, labelpad=20)
+                # scale cbar down
+                ax_local[0,0].set_title(f"{os.path.basename(args.image).split('.')[0]}", fontsize=16)
+                ax_local[1,0].set_title(f"Probability of Rock using RF", fontsize=16)
+                ax_local[1,1].set_title(f"Predicted Rock", fontsize=16)
+                ax_local[0,1].set_title(f"Preprocessed Image", fontsize=16)
+
+                # draw circles on rock labels
+                labels, ngroups = label(y_prob>0.6)
+                sizes = []
+                probs = []
+                # for each group draw a rectangle
+                for g in range(1,ngroups+1):
+                    rock_mask = labels==g
+                    # skip if smaller than a pixel
+                    if np.sum(rock_mask) < 2:
+                        continue
+
+                    # get bounding box
+                    y,x = np.where(rock_mask)
+
+                    radius = np.sqrt((x.max()-x.min()+1)*(y.max()-y.min()+1))
+
+                    # estimate size + probability
+                    size = np.sum(rock_mask)
+                    sizes.append(size)
+                    probs.append(np.max(y_prob[y,x]))
+
+                    # estimate color based on probability
+                    color_scale = np.max(y_prob[y,x])
+                    color = plt.cm.jet(color_scale)
+
+                    # plot circle based on size of bounding box
+                    ax_local[1,1].plot((x.min()+x.max())*0.5, (y.min()+y.max())*0.5, 'o', color=color, markersize=radius, alpha=0.5, fillstyle='none')
+
+                # collect data into bins of probability
+                pbins = np.linspace(0.5,1,6) # bins of probability
+                size_bins = [[] for ii in range(len(pbins)-1)]
+                # loop over sizes and put into probability bins
+                for ii in range(len(sizes)):
+                    for jj in range(len(pbins)-1):
+                        if probs[ii] >= pbins[jj] and probs[ii] < pbins[jj+1]:
+                            size_bins[jj].append(sizes[ii])
+                            break
+
+                # create stacked histogram with colors for intervals of probability
+                ax_local[2,0].hist(size_bins, bins=np.arange(20), stacked=True, 
+                                    color=plt.cm.jet(np.linspace(0.5,1,len(pbins)-1)), 
+                                    label=[f"{pbins[ii]:.2f} - {pbins[ii+1]:.2f}" for ii in range(len(pbins)-1)])
+
+                ax_local[2,0].legend()
+                ax_local[2,0].set_xlabel('Size of Rock Shadow [px]', fontsize=14)
+                ax_local[2,0].set_ylabel('Count', fontsize=14)
+                ax_local[2,0].set_title(f"Histogram of Rock Sizes", fontsize=16)
+
+                # remove last axis
+                ax_local[2,1].axis('off')
+                plt.tight_layout()
+                fname = os.path.join(outdir, f"{i}_{j}_prediction.png")
+                plt.savefig(fname, dpi=300)
+                plt.close()
+                print(f"Saved {fname}")
+
+    # save final rock mask
+    fname = os.path.join(outdir, f"rock_mask_{args.res}.png")
+    cv2.imwrite(fname, mask.astype(np.uint8)*255)
+    print(f"Saved {fname}")
---
+++ mask2density.py
@@ -0,0 +1,160 @@
+import os
+os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = pow(2,40).__str__()
+import cv2
+import pickle
+import glymur
+import argparse
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from skimage.measure import regionprops
+from scipy import spatial
+from skimage.transform import resize, downscale_local_mean
+from scipy.ndimage import binary_dilation, binary_closing, binary_erosion, label
+from itertools import cycle
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--image", type=str, default="images/PSP_001410_2210_RED_A_01_ORTHO.JP2",
+            help="Choose a JPEG2000 image to decode")
+
+    parser.add_argument("-o", "--outdir", type=str, default="output/", help="Directory to save outputs")
+
+    parser.add_argument("-th", "--threads", default=4, type=int,
+            help="number of threads for reading in JP2000 image")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # open image
+    args = parse_args()
+
+    # image loading options
+    glymur.set_option('lib.num_threads', args.threads)
+
+    # set up output
+    outdir = os.path.join(args.outdir, os.path.splitext(os.path.basename(args.image))[0])
+
+    # read in boolean mask for rocks
+    mask = cv2.imread(os.path.join(outdir,"rock_mask_0.png"), cv2.IMREAD_GRAYSCALE)
+    mask = mask.astype(bool)
+
+    # open jp2k image
+    image = glymur.Jp2k(args.image).read(rlevel=0).astype(np.float32)
+    non_zero_count = np.count_nonzero(image)
+
+    # clean mask with binary operations
+    mask = binary_closing(mask)
+    mask = mask & binary_dilation(binary_erosion(mask),iterations=3)
+    # Even just a single binary erosion operation eliminates any cluster 
+    # of points smaller than 2x3 and hence why we see the density drop off 
+    # so drastically. Binary dilation in combination with a logical and on 
+    # the original mask preserves the boundary shape of smaller segments. 
+    # This is converse to just doing an erosion followed by a dilation, which 
+    # tends to inflate the boundary uniformly in all directions and tends to 
+    # make the rock shapes more homogeneous at smaller sizes since you lose 
+    # information about the edges and boundary. Dilating the mask too much 
+    # will start to produce diamond shape masks as well. 
+
+    # save cleaned mask to output directory
+    cv2.imwrite(os.path.join(outdir,"rock_mask_cleaned.png"), mask.astype(np.uint8)*255)
+
+    # start counting rocks
+    label_image, ngroups = label(mask)
+    regions = regionprops(label_image)
+
+    # lists to store size of each rock
+    rock_data = {
+        "pixel_sizes": [],    # number of pixels in rock
+        "ellipse_area": [],   # area of ellipse that fits rock
+        "rock_locations": [], # location of rock in pixels
+    }
+
+    # compute size of each rock
+    for region in tqdm(regions):
+
+        # filter out small rocks
+        if region.area <= 2:
+            continue
+
+        # compute area of ellipse
+        area = region.axis_major_length * region.axis_minor_length # * np.pi
+        # The addition of pi is too much, the sizes don't make sense...
+
+        # save data to dict
+        rock_data["pixel_sizes"].append(region.area)
+        rock_data["ellipse_area"].append(area)
+        rock_data["rock_locations"].append(region.centroid)
+
+    # cast as numpy arrays
+    pixel_sizes = np.array(rock_data["pixel_sizes"])
+    ellipse_area = np.array(rock_data["ellipse_area"])
+    rock_locations = np.array(rock_data["rock_locations"])
+
+    # compute some metrics
+    area_per_pixel = 0.3**2 # m^2
+    total_area = non_zero_count * area_per_pixel
+    total_rock_area = np.sum(pixel_sizes) * area_per_pixel
+    total_ellipse_area = np.sum(ellipse_area) * area_per_pixel
+
+    print(f"Mean size of rock shadow: {np.mean(ellipse_area):.2f} px")
+    print(f"Median size of rock shadow: {np.median(ellipse_area):.2f} px")
+    print(f"Standard deviation of rock shadow size: {np.std(ellipse_area):.2f} px")
+    print(f"Max size of rock shadow: {np.max(ellipse_area):.2f} px")
+    print(f"Min size of rock shadow: {np.min(ellipse_area):.2f} px")
+    print(f"Number of rock shadows: {len(ellipse_area)}")
+    print(f"Total area of rock shadows: {total_rock_area:.2f} m^2")
+    print(f"Total area of rock shadows (ellipse): {total_ellipse_area:.2f} m^2")
+    print(f"Total area of image: {total_area:2f} m^2")
+    print(f"Percent of image covered by rock (pixel): {total_rock_area/total_area*100:.2f}%")
+    print(f"Percent of image covered by rock (ellipse): {total_ellipse_area/total_area*100:.2f}%")
+    # compute rock density per square km
+    print(f"Rock density (pixel): {len(rock_data['pixel_sizes'])/total_area*1e6:.0f} rocks/km^2")
+    print(f"Rock density (pixel): {len(rock_data['pixel_sizes'])/total_area*1e4:.0f} rocks/100m^2")
+    # compute average distance between rocks
+    print(f"Average distance between rocks: {np.sqrt(total_area/len(rock_data['pixel_sizes'])):.2f} m")
+    print(f"Average distance between rocks: {np.sqrt(total_area/len(rock_data['pixel_sizes'])/0.3):.2f} px")
+
+    # save rock data
+    rock_data['area_per_pixel'] = area_per_pixel
+    rock_data['total_image_area'] = total_area
+    rock_data['total_rock_area'] = total_rock_area
+    rock_data['total_ellipse_area'] = total_ellipse_area
+    rock_data['rock_density_km2'] = len(rock_data['pixel_sizes'])/total_area*1e6
+    rock_data['rock_density_100m2'] = len(rock_data['pixel_sizes'])/total_area*1e4
+    rock_data['average_rock_distance_m'] = np.sqrt(total_area/len(rock_data['pixel_sizes']))
+
+    # plot histogram of sizes
+    fig,ax = plt.subplots(1, 2, figsize=(8, 5))
+    counts, bins, _ = ax[0].hist(ellipse_area*area_per_pixel, bins=np.linspace(0.5,2.5,9), alpha=0.5, label='Ellipse Area')
+    ax[0].set_xlabel(r'Area of Rock (m$^2$)')
+    ax[0].set_title('Rock Size Distribution')
+    dbin = np.diff(bins).mean()
+    cfa = counts*(bins[:-1]+dbin)*area_per_pixel/total_area*100
+
+    # fit quadratic to density data and extrapolate to smaller sizes
+    x = bins[1:-1]
+    y = counts[1:]*(bins[1:-1]+dbin)/total_area*1e4
+    p = np.polyfit(x, y, 1)
+    xfit = np.linspace(0.25, 2.5, 100)
+    yfit = np.polyval(p, xfit)
+
+    # plot density per square 100m
+    ax[1].plot(bins[:-1], counts*(bins[:-1]+dbin)/total_area*1e4,'ko')
+    ax[1].plot(xfit, yfit, 'r--', label=f'Linear Fit (y = {p[0]:.2f}x + {p[1]:.2f})')
+    ax[1].legend(loc='best')
+    ax[1].set_xlabel(r'Area of Rock (m$^2$)')
+    ax[1].grid(True, ls='--')
+    ax[1].set_ylabel(r'Density (Rocks per 100x100 m$^2$)')
+    ax[1].set_xlim([0.25,2.5])
+    ax[1].set_ylim([0, 90])
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir,'rock_density.png'))
+    plt.close()
+
+
+    # save pickle of rock data
+    with open(os.path.join(outdir,'rock_data.pkl'), 'wb') as f:
+        pickle.dump(rock_data, f)
